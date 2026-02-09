84700e140b694f7b443ae6f5501d85b9adcd3d4e
vtu26270 <vtu26270@veltech.edu.in>
2026-02-09 09:42:34 +0530

    Time: 1 ms (99.75%), Space: 47.8 MB (53.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+
+        // Move non-zero elements forward
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        // Fill remaining positions with zeroes
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
+
